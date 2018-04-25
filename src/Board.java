4bb4083ffb5e9e85e5c8877839a1cfda36c2dfc5
WonderJ13 <wonderj013@yahoo.com>
2018-04-25 07:22:40 -0500

    added some comments

--- src/Board.java
+++ src/Board.java
@@ -1,22 +1,25 @@
-import java.util.ArrayList;
+import java.util.ArrayList; //Just an array that'll increase in size automatically
 public class Board
 {
-	private ArrayList<Wire> wires;
+	private ArrayList<Wire> wires; //Instance Variable: list of wires
+	/* <Wire> means the arraylist will accept objects of type Wire 
+	 * This can easily be changed to <Integer> or <String> to fit
+	 * any object you want */
 	
-	public Board()
+	public Board() //Constructor
 	{
 		wires = new ArrayList<Wire>();
 	}
 	
-	public void addWire(int x, int y) {
+	public void addWire(int x, int y) { //Public method: creates a new wire and puts it in the list
 		wires.add(new Wire(x, y, 5, 0));
 	}
 	
-	public int amountOfLines() {
+	public int amountOfLines() { //Public method: returns the size of the arraylist (how many wires are there?)
 		return wires.size();
 	}
 	
-	public Wire getWire(int index) {
+	public Wire getWire(int index) { //Returns a specific wire specified by the index
 		return wires.get(index);
 	}
 }
--- src/Wire.java
+++ src/Wire.java
@@ -1,36 +1,36 @@
 public class Wire
 {
-	private int x, y, length, dir;
-	private Wire con0, con1;
+	private int x, y, length, dir; //Instance Variables
+	private Wire con0, con1; //More Instance Variables
 	
-	public Wire(int x, int y, int length, int dir) {
+	public Wire(int x, int y, int length, int dir) { //Constructor
 		this.x = x;
 		this.y = y;
 		this.length = length;
 		this.dir = dir;
 	}
 	
-	public void connectWire0(Wire wire) {
+	public void connectWire0(Wire wire) { //public method: available to use by other classes
 		con0 = wire;
 	}
 	
-	public void connectWire1(Wire wire) {
+	public void connectWire1(Wire wire) { //public method: available to use by other classes
 		con1 = wire;
 	}
 	
-	public int getX() {
+	public int getX() { //public method: returns x pos
 		return x;
 	}
 	
-	public int getY() {
+	public int getY() { //public method: returns y pos
 		return y;
 	}
 	
-	public int getLength() {
+	public int getLength() { //public method: returns length of wire
 		return length;
 	}
 	
-	public int getDirection() {
+	public int getDirection() { //public method: returns direction
 		return dir;
 	}
 }
